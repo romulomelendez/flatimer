9cf476fc3e9426581e88a58087c397a83a68d633
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2023-11-12 20:33:10 -0300

    Feat: adding Flamengos's logo

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,3 +1,7 @@
+import Image from "next/image"
+
+import flamengoLogo from "../../public/assets/flamengo-logo.png"
+
 export const Timer: React.FC = () => {
 
     return (
@@ -7,7 +11,8 @@ export const Timer: React.FC = () => {
         <h1 className="text-white font-semibold text-6xl">24 minutos</h1>
         <h1 className="text-white font-semibold text-6xl">3 segundos</h1>
 
-        <h4 className="text-red-800 font-normal text-4xl mt-5">Desde o último título</h4>
+        <h4 className="text-red-800 font-normal text-4xl mt-5">Desde o último título do</h4>
+        <Image src={flamengoLogo} alt="Flamengo's Logo" className="mt-5 w-80 " />
       </section>
     )
   
