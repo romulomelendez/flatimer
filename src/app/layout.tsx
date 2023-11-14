e97123b8d98966672c0288eaf6761e66cefae215
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2023-11-13 21:56:27 -0300

    🛠️ Fix: suppressHydrationWarning prop set true to fix console warning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-br">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={inter.className}
+        suppressHydrationWarning={true}
+      >
+        {children}
+      </body>
     </html>
   )
 }
