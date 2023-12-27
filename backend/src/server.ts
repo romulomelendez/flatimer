600fd4b40307b08793833379d8ba02cfad83d5e2
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2023-12-27 19:19:19 -0300

    backend: env.sample file created

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,6 @@ import { app } from './app'
 
 import 'dotenv/config'
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3331
 
 app.listen(port, () => console.log(`🚀 Server listening on port: ${port}`))
