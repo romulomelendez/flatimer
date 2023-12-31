aa62a31ff3ac6f4f28b3fbfe3247c8647b7c67bb
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2023-12-31 15:56:46 -0300

    feat: formating club routes

---
+++ backend/src/main/routes/club-routes.ts
@@ -0,0 +1,52 @@
+import { Request, Response, Router } from "express"
+import { PrismaClient } from "@prisma/client"
+
+export const userRoutes = Router()
+
+userRoutes.get("/api/club/allClubs", async (_, res: Response) => {
+
+    const prisma = new PrismaClient()  
+    const clubs = await prisma.club.findMany()
+
+    if(!clubs)
+        return res.status(404).json("No one club found")
+    
+    res.status(200).json(clubs)
+})
+
+userRoutes.get("/api/club/name/:clubName", async (req: Request, res: Response) => {
+
+    const { clubName } = req.params
+
+    const prisma = new PrismaClient()
+    const club = await prisma.club.findUnique({
+        where: {
+            name: clubName
+        }
+    })
+
+    if(!club)
+        res.status(404).json("Club Not Found!")
+
+    res.status(200).json(club)
+})
+
+userRoutes.get("/api/club/id/:clubId", async (req: Request, res: Response) => {
+
+    const { clubId } = req.params
+
+    const prisma = new PrismaClient()
+    const club = await prisma.club.findUnique({
+        where: {
+            id: +clubId
+        }
+    })
+
+    if(!club)
+        return res.status(404).json("Club not found!")
+
+    return res.status(200).json({
+        message: "Club successfully found!",
+        body: club
+    })
+})
