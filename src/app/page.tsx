42fe47e6c444d77782c691097fdb6fd0c7e7c860
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2024-01-11 22:02:52 -0300

    Feat: frontend => timer is working again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ const Home: React.FC = () => (
   <div className="bg-slate-900 h-screen w-screen flex flex-col justify-center items-center p-3 gap-2">
     <Timer />
     <h4 className="text-red-800 font-normal text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl mt-5">
-      Desde o último título do
+      Desde o último título do:
     </h4>
     <Image
       src={flamengoLogo}
--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,47 +1,90 @@
-"use client";
+"use client"
 
-import { Suspense, useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react"
 
-import { TimerUnit } from "./TimerUnit";
+import { TimerUnit } from "./TimerUnit"
 
 type LastTitleProps = {
-  dateOfLastTitle?: string,
-  years: string | number,
-  mounths: string | number,
-  days: string | number,
-  hours: string | number,
-  minutes: string | number,
-  seconds: string | number 
+  "id": number,
+  "name": string,
+  "dateOfLastTitle": number,
+  "years": number,
+  "mounths": number,
+  "days": number,
+  "hours": number,
+  "minutes": number,
+  "seconds": number,
+  "clubId": number
 }
 
-export const Timer: React.FC = () => {
 
-  const [timerUnitValues, setTimerUnitValues] = useState<LastTitleProps>({} as LastTitleProps);
+export const Timer: React.FC = () => {
+  const [timerUnitValues, setTimerUnitValues] = useState<LastTitleProps>({} as LastTitleProps)
 
   useEffect(() => {
     return () => {
       const handleTimerUnits = async (): Promise<void> => {
-        const soccer_team = process.env.NEXT_PUBLIC_SOCCER_TEAM as string;
-        const timerUnitsResponse = await (await fetch(process.env.NEXT_PUBLIC_BASE_API_URL + soccer_team)).json();
-        setTimerUnitValues(timerUnitsResponse);
-      };
-      handleTimerUnits();
-    };
-  }, []);
-  
+        const soccer_team = process.env.NEXT_PUBLIC_SOCCER_TEAM as string
+        const timerUnitsResponse = await (
+          await fetch(process.env.NEXT_PUBLIC_BASE_API_URL + soccer_team)
+        ).json()
+        setTimerUnitValues(timerUnitsResponse.lastTitleDate)
+      }
+      handleTimerUnits()
+    }
+  }, [])
+
   return (
-    <section className="flex flex-col gap-1 justify-center items-center p-2">
+    <section className="flex flex-col gap-4 justify-center items-center p-2">
       <div className="grid grid-cols-3 grid-rows-2 gap-9 2xl:flex 2xl:gap-12 w-full">
         <Suspense fallback={<h2>🌀Loading...</h2>}>
-          <TimerUnit unit={{ unitTimerValue: timerUnitValues.years, one: "ano", moreThanOne: "anos"}} />
-          <TimerUnit unit={{ unitTimerValue: timerUnitValues.mounths, one: "mês", moreThanOne: "meses"}} />
-          <TimerUnit unit={{ unitTimerValue: timerUnitValues.days, one: "dia", moreThanOne: "dias"}} />
-          <TimerUnit unit={{ unitTimerValue: timerUnitValues.hours, one: "hora", moreThanOne: "horas"}} />
-          <TimerUnit unit={{ unitTimerValue: timerUnitValues.minutes, one: "minuto", moreThanOne: "minutos"}} />
-          <TimerUnit unit={{ unitTimerValue: timerUnitValues.seconds, one: "segundo", moreThanOne: "segundos"}} />
+          <TimerUnit
+            unit={{
+              unitTimerValue: timerUnitValues.years,
+              one: "ano",
+              moreThanOne: "anos",
+            }}
+          />
+          <TimerUnit
+            unit={{
+              unitTimerValue: timerUnitValues.mounths,
+              one: "mês",
+              moreThanOne: "meses",
+            }}
+          />
+          <TimerUnit
+            unit={{
+              unitTimerValue: timerUnitValues.days,
+              one: "dia",
+              moreThanOne: "dias",
+            }}
+          />
+          <TimerUnit
+            unit={{
+              unitTimerValue: timerUnitValues.hours,
+              one: "hora",
+              moreThanOne: "horas",
+            }}
+          />
+          <TimerUnit
+            unit={{
+              unitTimerValue: timerUnitValues.minutes,
+              one: "minuto",
+              moreThanOne: "minutos",
+            }}
+          />
+          <TimerUnit
+            unit={{
+              unitTimerValue: timerUnitValues.seconds,
+              one: "segundo",
+              moreThanOne: "segundos",
+            }}
+          />
         </Suspense>
       </div>
+      <h4 className="text-white text-2xl sm:text-3xl">
+        { timerUnitValues.name } - { timerUnitValues.dateOfLastTitle }
+      </h4>
     </section>
-  );
-  
-};
+  )
+}
